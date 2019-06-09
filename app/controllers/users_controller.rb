87b0f3f23094fe75d06b9b0e1750d8935b0dc21f
Tafang Joshua <tafodinho@gmail.com>
2019-06-09 17:43:17 +0100

    implemented basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  def index
+
+  end
+
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user =   User.new(user_params)
+
+    if @user.save
+      login @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    elsif
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :passowrd_confirmation)
   end
 end

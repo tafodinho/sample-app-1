946115f94586984dbac189895948a098a377ada4
Tafang Joshua <tafodinho@gmail.com>
2019-06-11 22:24:41 +0100

    Implemented advanced login without completing the test on remembering passwords

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,13 @@ module SessionsHelper
     end
 
     def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
+        if user_id = session[:user_id]
+            @current_user ||= User.find_by(id: user_id)
+        elsif user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user 
+                @current_user = user
+            end
         end
     end
 
@@ -14,8 +19,21 @@ module SessionsHelper
         !current_user.nil?
     end
 
+    def forget(user)
+        user.forget_user
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end 
+
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    def remember(user)
+        user.remember 
+        cookies.permanent.signed[:user_id] = user.id 
+        cookies.permanent[:remember_token] = user.remember_token
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
+
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: {maximum: 50}
@@ -11,5 +13,24 @@ class User < ApplicationRecord
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                         BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    def User.new_token
+        SecureRandom.urlsafe_base64                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    end
+
+    def remember 
+        self.remember_token = User.new_token 
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest) == remember_token
+        # BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget_user 
+        update_attribute(:remember_digest, nil)
     end
 end

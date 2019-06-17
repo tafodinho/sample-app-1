3d020ec462a17b6071e1ee22b4dc78a6e2c8314b
Tafang Joshua <tafodinho@gmail.com>
2019-06-17 09:37:17 +0100

    Added password resets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     before_save :downcase_email
     before_create :create_activation_digest
+
 
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
@@ -42,6 +43,20 @@ class User < ApplicationRecord
 
     def send_activation_email 
         UserMailer.account_activation(self).deliver_now
+    end
+
+    def create_reset_digest 
+        self.reset_token = User.new_token 
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end 
+
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 
     private 
